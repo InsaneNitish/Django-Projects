ab9e86338ea86bece603a1297b1153301ddff12f
Insanenitish <nitishtthakur786@gmail.com>
2023-11-27 19:50:16 +0530

    1. added add to cart button 2. added username on profile when logged in

--- eCommerce/models.py
+++ eCommerce/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     product_name=models.CharField(max_length=30)
     product_price=models.IntegerField()
-    product_description=models.CharField(max_length=50)
+    product_description=models.TextField(blank=True)
     rating=models.FloatField()
-    product_img=models.ImageField(upload_to='static/images')
+    product_img=models.ImageField(upload_to='images/',blank=True,null=True)
     product_category=models.CharField(max_length=20)
     product_tag=models.CharField(max_length=30)
     product_id=models.IntegerField()
@@ -14,4 +14,6 @@ class Product(models.Model):
     def __str__(self) -> str:
         return self.product_name + " " + self.product_description
     
+
+
 # Create your models here.
